4fd962d0d27147c911f81d4b74009fe44414e98e
Ali Rehman <133681797+alirehman765@users.noreply.github.com>
2025-01-26 14:53:27 +0500

    Update order-controller.js

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -16,76 +16,99 @@ const createOrder = async (req, res) => {
       orderDate,
       orderUpdateDate,
       paymentId,
-      payerId,
+       payerId,
       cartId,
     } = req.body;
 
-    const create_payment_json = {
-      intent: "sale",
-      payer: {
-        payment_method: "paypal",
-      },
-      redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
-      },
-      transactions: [
-        {
-          item_list: {
-            items: cartItems.map((item) => ({
-              name: item.title,
-              sku: item.productId,
-              price: item.price.toFixed(2),
-              currency: "USD",
-              quantity: item.quantity,
-            })),
-          },
-          amount: {
-            currency: "USD",
-            total: totalAmount.toFixed(2),
-          },
-          description: "description",
-        },
-      ],
-    };
+    // const create_payment_json = {
+    //   intent: "sale",
+    //   payer: {
+    //     payment_method: "paypal",
+    //   },
+    //   redirect_urls: {
+    //     return_url: "http://localhost:5173/shop/paypal-return",
+    //     cancel_url: "http://localhost:5173/shop/paypal-cancel",
+    //   },
+    //   transactions: [
+    //     {
+    //       item_list: {
+    //         items: cartItems.map((item) => ({
+    //           name: item.title,
+    //           sku: item.productId,
+    //           price: item.price.toFixed(2),
+    //           currency: "PKR",
+    //           quantity: item.quantity,
+    //         })),
+    //       },
+    //       amount: {
+    //         currency: "PKR",
+    //         total: totalAmount.toFixed(2),
+    //       },
+    //       description: "description",
+    //     },
+    //   ],
+    // };
 
-    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
-      if (error) {
-        console.log(error);
+    // paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+    //   if (error) {
+    //     console.log(error);
 
-        return res.status(500).json({
-          success: false,
-          message: "Error while creating paypal payment",
-        });
-      } else {
-        const newlyCreatedOrder = new Order({
-          userId,
-          cartId,
-          cartItems,
-          addressInfo,
-          orderStatus,
-          paymentMethod,
-          paymentStatus,
-          totalAmount,
-          orderDate,
-          orderUpdateDate,
-          paymentId,
-          payerId,
-        });
+    //     return res.status(500).json({
+    //       success: false,
+    //       message: "Error while creating paypal payment",
+    //     });
+    //   } else {
+    //     const newlyCreatedOrder = new Order({
+    //       userId,
+    //       cartId,
+    //       cartItems,
+    //       addressInfo,
+    //       orderStatus,
+    //       paymentMethod,
+    //       paymentStatus,
+    //       totalAmount,
+    //       orderDate,
+    //       orderUpdateDate,
+    //       // paymentId,
+    //       // payerId,
+    //     });
 
-        await newlyCreatedOrder.save();
+    //     await newlyCreatedOrder.save();
 
-        const approvalURL = paymentInfo.links.find(
-          (link) => link.rel === "approval_url"
-        ).href;
+    //     const approvalURL = paymentInfo.links.find(
+    //       (link) => link.rel === "approval_url"
+    //     ).href;
 
-        res.status(201).json({
-          success: true,
-          approvalURL,
-          orderId: newlyCreatedOrder._id,
-        });
-      }
-    });
+    //     res.status(201).json({
+    //       success: true,
+    //       approvalURL,
+    //       orderId: newlyCreatedOrder._id,
+    //     });
+    //   }
+    // });
+
+    const newlyCreatedOrder = new Order({
+            userId,
+            cartId,
+            cartItems,
+            addressInfo,
+            orderStatus,
+            paymentMethod,
+            paymentStatus,
+            totalAmount,
+            orderDate,
+            orderUpdateDate,
+            paymentId,
+            payerId,
+          });
+          await newlyCreatedOrder.save();
+
+          res.status(201).json({
+                  success: true,
+      
+                  orderId: newlyCreatedOrder._id,
+                  order: newlyCreatedOrder,
+                });
   } catch (e) {
     console.log(e);
     res.status(500).json({
