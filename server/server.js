dc43b79d96f92c4359aaffee2c279ef33013962f
Ali Rehman <133681797+alirehman765@users.noreply.github.com>
2025-05-13 23:47:46 +0500

    Update server.js for the mentor , mentorship routes added also virtual try on route added

--- server/server.js
+++ server/server.js
@@ -3,23 +3,29 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
+const shopOrderRouter = require("./routes/shop/order-routes"); 
 const adminProductsRouter = require("./routes/admin/products-routes");
-
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
-
 const commonFeatureRouter = require("./routes/common/feature-routes");
-
-
-mongoose.connect("mongodb+srv://mominafaisal59:MominaFaisal2003@empowerher.hrvsf.mongodb.net/")
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
+const paymentRouter = require("./routes/shop/payment-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+const mentorRouter = require('./routes/mentor');
+const mentorshipRouter = require('./routes/mentorship');
+const tryonRoutes = require('./routes/shop/tryon-routes'); 
+// const fileUpload = require('express-fileupload');
+// const pixelcutRouter = require('./routes/shop/pixelcut-routes');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI )
+  .then(() => console.log("Connected to DATABASE"))
+  .catch((error) => console.log("MongoDB connection error:", error));
 
 app.use(
   cors({
@@ -40,13 +46,23 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
-
+app.use("/api/admin/orders", adminOrderRouter);
 app.use("/api/shop/products", shopProductsRouter);
+// app.use('/api/shop/pixelcut', pixelcutRouter); // Add this line to include the Pixelcut routes
+app.use('/api/shop', tryonRoutes);
+// app.use(fileUpload({
+//   useTempFiles: true,
+//   tempFileDir: '/tmp/',
+// }));
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
-
 app.use("/api/common/feature", commonFeatureRouter);
+app.use("/api/shop/payment", paymentRouter);
+app.use('/api/mentor', mentorRouter);
+app.use('/api/mentorship', mentorshipRouter);
+
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
